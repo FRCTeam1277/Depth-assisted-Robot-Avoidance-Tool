4a4f2ec91375035ccf9cfe5cd79a93ae2c101e5b
chillipenguin <achillipenguin@gmail.com>
2024-08-27 12:45:14 -0400

    Added doc string

--- AStarSetup.py
+++ AStarSetup.py
@@ -1,4 +1,6 @@
 """ Converts AStar algorithm into c code to make it way more efficent
+Use `python setup.py build_ext --inplace` to run (DON'T RUN DIRECTLY)
+Run this on the raspberry pi to have it built properly!
 """
 from setuptools import setup
 from Cython.Build import cythonize
